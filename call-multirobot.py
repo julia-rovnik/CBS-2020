fa1d005f6dfdb00a4d292352a7f89141064b9139
Julia Rovnik <32664910+julia-rovnik@users.noreply.github.com>
2020-06-02 16:53:32 +0300

    Update call-multirobot.py

--- call-multirobot.py
+++ call-multirobot.py
@@ -2,64 +2,50 @@ import os
 import time
 import subprocess
 
-MAX_AGENTS = 100
-MAX_SCEN = 26
-MAX_FAILED = 4
-TIME_OUT = 30
+path_to_bin = "try-multirobot" # path to the binary
+path_to_map = "brc202d.map" # path to the map
+paths_to_scen = ["brc202d-even-1",
+                 "brc202d-even-2",
+                 "brc202d-even-3"] # paths to scenarios
 
-#code = "/Users/clarence/Desktop/multirobot/code/try-multirobot" # path to code
-#cmap = "/Users/clarence/Desktop/multirobot/maps/brc202d.map" # path to map
-#scen = "/Users/clarence/Desktop/multirobot/maps/brc202d-scen-even/brc202d-even-" # template path to scenarios
+MAX_AGENTS = 10 # maximum number of agents
+MAX_SCEN = len(paths_to_scen) # maximum number of scenarios
+MAX_FAILED = 4 # maximum consecutive failures
+TIME_OUT = 30 # time limit
 
-dijkstra_precalc = "true"
-use_CAT = "true"
-heuristic = "normal" # (normal, number_of_conflicts, number_of_conflicting_agents, number_of_pairs, vertex_cover)
-prioritize_conflicts = "false"
-use_bypass = "false"
-use_ecbs = "false"
-omega = "1.1"
+dijkstra_precalc = "true" # true -> use dijkstra precalc
+use_CAT = "true" # true -> use CAT
+heuristic = "normal" # heuristic (normal, number_of_conflicts, number_of_conflicting_agents, number_of_pairs, vertex_cover)
+prioritize_conflicts = "false" # true -> prioritize conflicts
+use_bypass = "false" # true -> use bypass
+use_ecbs = "false" # true -> use ecbs
+omega = "1.1" # suboptimality factor
 
-#code = "/Users/clarence/Desktop/try-multirobot"
-#cmap = "/Users/clarence/Desktop/multirobot/maps/room-64-64-8.map"
-#scen = "/Users/clarence/Desktop/multirobot/maps/room-scen-even/room-64-64-8-even-"
+success_rates = [] # percent of passed tests for each number of agents
+failed = [0] * MAX_SCEN # for counting consecutive failures
 
-#code = "/Users/clarence/Desktop/try-multirobot"
-#cmap = "/Users/clarence/Desktop/multirobot/maps/warehouse-10-20-10-2-2.map"
-#scen = "/Users/clarence/Desktop/multirobot/maps/warehouse-scen-even/warehouse-10-20-10-2-2-even-"
+for agents in range(1, MAX_AGENTS): # choose a number of agents
+    num_of_passed_tests = 0
 
-#code = "/Users/clarence/Desktop/try-multirobot"
-#cmap = "/Users/clarence/Desktop/multirobot/maps/empty-16-16.map"
-#scen = "/Users/clarence/Desktop/multirobot/maps/empty-16-16-scen-even/empty-16-16-even-"
-
-#code = "/Users/clarence/Desktop/try-multirobot"
-#cmap = "/Users/clarence/Desktop/multirobot/maps/empty-8-8.map"
-#scen = "/Users/clarence/Desktop/multirobot/maps/empty-8-8-scen-even/empty-8-8-even-"
-
-answer = []
-failed = [0] * MAX_SCEN
-
-for agents in range(1, MAX_AGENTS):
-    good = 0
-    for num in range(1, MAX_SCEN):
-        if (failed[num] <= MAX_FAILED):
+    for cur_scen in range(0, MAX_SCEN): # choose a scenario
+        if (failed[cur_scen] <= MAX_FAILED): # if the number of consecutive failures doesn't exceed MAX_FAILED
+            # start the timer and create a subprocess
             timeStarted = time.time()
-            test = subprocess.Popen([code, cmap, scen + str(num) + ".scen", str(agents), dijkstra_precalc, use_CAT, heuristic, prioritize_conflicts, use_bypass, use_ecbs, omega])
+            test = subprocess.Popen([path_to_bin, path_to_map, paths_to_scen[cur_scen], str(agents), dijkstra_precalc, use_CAT, heuristic, prioritize_conflicts, use_bypass, use_ecbs, omega])
 
             try:
                 test.wait(timeout = TIME_OUT)
             except subprocess.TimeoutExpired:
                 test.kill()
-                failed[num] += 1
-                print("ag:", agents, "test:", num, "res: BAD")
+                failed[cur_scen] += 1
+                timeDelta = time.time() - timeStarted
+                print("agents:", agents, "scenario:", cur_scen, "time:", "%.5f" % timeDelta, "result: FAILURE") # time limit exceeded
             else:
-                good += 1
-                print("ag:", agents, "test:", num, "res: GOOD")
-
-            timeDelta = time.time() - timeStarted
-            print("%.5f" % timeDelta)
-
+                num_of_passed_tests += 1
+                timeDelta = time.time() - timeStarted
+                print("agents:", agents, "scenario:", cur_scen, "time:", "%.5f" % timeDelta, "result: SUCCESS")
         else:
-            print("ag:", agents, "test:", num, "res: BAD")
+            print("agents:", agents, "scenario:", cur_scen, "time:", "MAX_FAILED exceeded", "result: FAILURE")
 
-    answer.append(good)
-    print(answer)
+    success_rates.append(round(num_of_passed_tests / MAX_SCEN * 100, 4))
+    print("\nsuccess rates for each number of agents:", success_rates, "\n")
